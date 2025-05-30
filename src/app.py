5a3c81eda58061ebb7959e2a081ced2da292a0c2
LunaB28 <lunab2815@gmail.com>
2025-05-30 23:42:58 +0000

    Final commit

--- src/app.py
+++ src/app.py
@@ -14,8 +14,15 @@ def hello_world():
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
     request_body=request.json
-    print('Incoming request with the following body', request_body)
-    return 'Response for the POST todo'
+    print("Incoming request with the following body", request_body)
+    todos.append(request_body)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print('this is the position to delete', position)
+    todos.pop(position)
+    return jsonify(todos)
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0', port=3245, debug=True)
+    app.run(host='0.0.0.0', port=3248, debug=True)
